b7d72104c43f479019e4a1ea155e22ad63873be1
MithanielVillard <mvillardhernandez@gaming.tech>
2025-04-15 17:00:15 +0200

    Updated matrix

--- matrix.py
+++ matrix.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 class Matrix:
      
-    def __init__(self):
-        self.matrix = []
+    def __init__(self, size : int):
+        self.matrix = [[0 for _ in range(size)] for _ in range(size)]
+        self.size = size
 
-    def __getitem__(self, i: int) -> list[list[float]]:
+    def __getitem__(self, i: int) -> list[float]:
         return self.matrix[i]
     
     def __str__(self):
@@ -17,7 +20,25 @@ class Matrix:
                 m[row][col] = self.matrix[col][row]
             
         return m
-    
-    def append(self, row : list[float]) -> list[list[float]]:
-        self.matrix.append(row)
-        return self.matrix
+
+    def pop(self, i : int, j : int) -> Matrix:
+        B = []
+        for k in range(self.size):
+            if k != i:
+                C = []
+                for l in range(self.size):
+                    if l != j:
+                        C.append(self.matrix[k][l])
+                B.append(C)
+        return B
+
+
+def det(matrix : Matrix) -> float:
+    j = 0
+    d = 0
+    if len(matrix.matrix) == 1:
+        return matrix[0][0]
+    else:
+        for i in range(len(matrix.matrix)):
+            d += (-1) ** (i + j) * matrix[i][j] * det(matrix.pop(i, j))
+        return d
